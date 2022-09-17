26400abb4d2d2f2d5dab84f2c255843fc2eebee5
Philip White <philip@mailworks.org>
2022-09-16 21:53:18 -0700

    Give fixed height to phrase and center it; don't jerk UI during animation (resolve #31)

--- src/Phrase.tsx
+++ src/Phrase.tsx
@@ -18,12 +18,22 @@ interface IProps {
 export class Phrase extends Component<IProps> {
   public render() {
     const orderedPhraseParts = this.props.phraseStruct.order;
+    /**
+     * Notes on visual implementation of phrase:
+     * I use a flexbox of a fixed height so that elements below the phrase don't jump around
+     * during animation.
+     * I use an inner div, stretched to 100% width, to ensure that multiple lines of the phrase
+     * don't stretch to the full height of flexbox. I did not find a setting of `align-items`
+     * that works without an inner div.
+     */
     return <div id="phrase">
-      {orderedPhraseParts.map((part, i) =>
-        <span key={this.props.ppUiProps[i].key}>
+      <div id="phrase-inner">
+        {orderedPhraseParts.map((part, i) =>
+            <span key={this.props.ppUiProps[i].key}>
           <PhrasePartUi phrasePart={part} ppUiProps={this.props.ppUiProps[i]}/>
         </span>
-      )}
+        )}
+      </div>
     </div>;
   }
 }
