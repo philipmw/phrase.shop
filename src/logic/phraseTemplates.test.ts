5e4251f064a9e8dca4e38de21f3406d1c18900ae
Philip White <philip@mailworks.org>
2022-09-19 21:36:55 -0700

    Add unit tests for phrase templates

---
+++ src/logic/phraseTemplates.test.ts
@@ -0,0 +1,30 @@
+import {makePhraseHard, makePhraseMedium, makePhraseSimple} from "./phraseTemplates";
+import {entropyNeededForPhrase} from "./phrase";
+import {visitTreeWithDependencies} from "./tree";
+
+describe("phrase templates", () => {
+  it("has expected entropy requirements", () => {
+    expect(entropyNeededForPhrase(makePhraseSimple())).toBe(24);
+    expect(entropyNeededForPhrase(makePhraseMedium())).toBe(40);
+    expect(entropyNeededForPhrase(makePhraseHard())).toBe(52);
+  });
+
+  it("has a properly structured dependency tree", () => {
+    const simplePhrase = makePhraseSimple();
+    expect(simplePhrase.order).toHaveLength(wordNodesInPhrase(simplePhrase));
+
+    const mediumPhrase = makePhraseMedium();
+    expect(mediumPhrase.order).toHaveLength(wordNodesInPhrase(mediumPhrase));
+
+    const hardPhrase = makePhraseHard();
+    expect(hardPhrase.order).toHaveLength(wordNodesInPhrase(hardPhrase));
+  });
+});
+
+function wordNodesInPhrase(phrase): number {
+  let seenWordNodes = 0;
+  visitTreeWithDependencies(phrase.depTree, () => {
+    ++seenWordNodes;
+  });
+  return seenWordNodes;
+}
